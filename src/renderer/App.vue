982d049998fb85bb30068b4e44de18bf17782654
zoar <zoar@k-side.net>
2020-10-30 09:35:06 +0900

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'threegoodthings-with-ryouka',
+    components: {
+      LandingPage
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
